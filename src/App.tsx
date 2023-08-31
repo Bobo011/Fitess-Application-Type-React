6e14b7b0974a439b774cf6e86dee7b246f061aff
George <alximik15@gmail.com>
2023-08-31 11:44:44 +0400

    Project Finished

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import {Home} from '@/scenes/home'
 import {Benefits} from '@/scenes/benefits'
 import { OurClasses } from '@/scenes/ourClasses'
 import {ContactUs} from '@/scenes/contactUs'
-import {Footer} from '@/scenes/footer'
+import Footer from '@/scenes/footer'
 
 function App() {
 
